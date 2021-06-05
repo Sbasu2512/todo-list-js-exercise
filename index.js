869fff367e2cf89afeb75d0ac00a7e3b4b3e6b42
Sayantan Basu <sayantanbasu@live.in>
2021-06-05 17:48:14 -0400

    updated the file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescription = [] ;
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescription = [] ;
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
@@ -21,14 +21,14 @@ complete: false
 }
 
 //Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(boolean) {
+function completeTask(task) {
   task.complete = true;
 }
 
 //Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
+function logTaskState(task) {
+  const title = task.title;
+  const complete = task.complete ;
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
 
